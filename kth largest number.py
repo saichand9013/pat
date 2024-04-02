b8a4452cb42b6c96c5f0e514a1a389bc294d34d4
saichand9013 <154426887+saichand9013@users.noreply.github.com>
2024-04-02 14:56:36 +0530

    Create kth largest number.py

---
+++ kth largest number.py
@@ -0,0 +1,13 @@
+#kth largest number
+import heapq
+def find_klargest(arr,k):
+    arr=[-item for item in arr]
+    heapq.heapify(arr)
+    while k>0:
+        s=heapq.heappop(arr)
+        k=k-1
+    return s*-1
+arr=list(map(int,input().split()))
+k= int(input())
+ans =find_klargest(arr,k)
+print(ans)
