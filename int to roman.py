5f2ca221db34376442772de592f30a54cd2cbfaa
saichand9013 <154426887+saichand9013@users.noreply.github.com>
2024-04-03 22:48:30 +0530

    Create int to roman.py

---
+++ int to roman.py
@@ -0,0 +1,14 @@
+#int to roman
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        r = {
+            1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL',
+            50: 'L', 90: 'XC', 100: 'C', 400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
+        }
+        l = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        res = ""
+        for i in l:
+            while num >= i:
+                res += r[i]
+                num -= i
+        return res
